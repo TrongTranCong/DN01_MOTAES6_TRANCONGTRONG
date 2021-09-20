ad64d7bf32ce7e18f23b46cd18f70f1309baef5d
TrongTranCong <87512260+TrongTranCong@users.noreply.github.com>
2021-09-20 16:03:11 +0700

    Update main.js

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -1,5 +1,8 @@
 //list color
 const colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
+let getEle = (id)=>{
+    return document.querySelector(id);
+}
 //loadColorList function using arrow fs 
 let loadColorList = () => {
     let content = " ";// nếu để trong map nó sẽ xuất hiện cái item cuối cùng
@@ -14,36 +17,10 @@ let loadColorList = () => {
 loadColorList();
 // Change colors 
 let changeColorHouse = (item)=>{
-    let newColor = "";
-    newColor += `<div id="house" class="house ${item} ${index = item ? 'active' : ''}">
-    <div class="house-base">
-        <div class="house-base-detail"></div>
-        <div class="house-door-wrapper">
-            <div class="house-door">
-                <div class="house-door-window"></div>
-                <div class="house-door-detail"></div>
-            </div>
-        </div>
-        <div class="house-window-wrapper">
-            <div class="house-window house-window-left"></div>
-            <div class="house-window house-window-right"></div>
-        </div>
-    </div>
-    <div class="house-sandwich"></div>
-    <div class="house-upper">
-        <div class="house-upper-detail"></div>
-        <div class="house-upper-shadow"></div>
-        <div class="house-window"></div>
-        <div class="house-window-wrapper">
-            <div class="house-window house-window-left"></div>
-            <div class="house-window house-window-right"></div>
-        </div>
-    </div>
-    <div class="house-roof">
-        <div class="house-roof-side house-roof-side-left"></div>
-        <div class="house-roof-side house-roof-side-right"></div>
-        <div class="house-roof-detail"></div>
-    </div>
-                </div>`;
-    document.getElementById("house").innerHTML = newColor;
+    let currentActive = getEle('.active');
+    console.log(currentActive);
+    currentActive && currentActive.classList.remove('active');
+    getEle(`.color-button.${item}`).classList.add('active');
+    getEle('#house').className = `house ${item}`;
 }
+loadColorList();
